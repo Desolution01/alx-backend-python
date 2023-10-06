977a2ca642f985fb7d7d5257757c29a5705b7667
Desolution01 <jas3610@gmail.com>
2023-10-06 02:37:22 +0100

    task 10 and 11

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-"""
-Augment the following code with the correct duck-typed annotations.
-"""
-from typing import Any, Sequence, Union
+""" Duck typing - first element of a sequence """
+
+from typing import Any, Union, Sequence, Iterable, List, Tuple
 
 
+# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''Retrieves the first element of a sequence if it exists.
-    '''
+    
+    """ Safe first element """
     if lst:
         return lst[0]
     else:
