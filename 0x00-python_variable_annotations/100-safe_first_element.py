f2e09ad3e2bf290fc6de0549dc9a86d8b0f238cf
Desolution01 <jas3610@gmail.com>
2023-10-06 02:21:18 +0100

    fixes for task 1,2,3,10 and 11

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
 
-"""
-Script defines a function to safely retrieve the first element from a list
-"""
-
-from typing import Sequence, Any, Union
+'''Task 10's module.
+'''
+from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    Get the first element from a list safely
-
-    Args:
-        lst (Sequence[Any]): The input list of elements of unknown types
-
-    Returns:
-        Union[Any, None]: First element of the list or None if list is empty
-    """
+    '''Retrieves the first element of a sequence if it exists.
+    '''
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
-
-"""
-This script defines a function to safely retrieve a value from a dictionary
-"""
-
-from typing import Mapping, Any, TypeVar, Union
+'''Task 11's module.
+'''
+from typing import Any, Mapping, Union, TypeVar
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
-    """
-    Safely retrieve a value from a dictionary
-
-    Args:
-        dct (Mapping): The input dictionary
-        key (Any): The key to look up in the dictionary
-        default: The default value to return if the key is not found
-
-    Returns:
-        The value associated with the key in the dict, or the default value
-    """
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    '''Retrieves a value from a dict using a given key.
+    '''
     if key in dct:
         return dct[key]
     else:
